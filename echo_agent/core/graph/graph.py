52af5725f9e9ef79453962b166c1ffe4a68aaa48
Jerry Yang <1622595386@qq.com>
2026-06-30 10:58:58 +0800

    更新了Graph类的实现，并优化docs文件夹结构

--- echo_agent/core/graph/graph.py
+++ echo_agent/core/graph/graph.py
@@ -1,12 +1,19 @@
-from langgraph.graph import CompiledGraph
-
 from node import Node, SubGraph
 from schema import BaseContext, BaseInput, BaseOutput, BaseState
 
 
 class Graph:
     """
-    Graph 类
+    Graph 类：用于定义图结构，包括节点、边、子图等。
+
+    参数：
+        state_schema: 状态
+        context_schema: 上下文
+        input_schema: 输入
+        output_schema: 输出
+        nodes: 节点列表
+        edges: 边列表
+        subgraphs: 子图列表
     """
     def __init__(self, state_schema: BaseState, context_schema: BaseContext, input_schema: BaseInput, output_schema: BaseOutput):
         self._state_schema = state_schema
@@ -14,14 +21,15 @@ class Graph:
         self._input_schema = input_schema
         self._output_schema = output_schema
 
-    def add_node(self, node: Node) -> None:
-        pass
+        self._nodes: dict[str, Node] = {}
+        self._edges: list[tuple[str, str]] = []
+        self._subgraphs: list[SubGraph] = []
 
-    def add_edge(self, from_node: Node, to_node: Node) -> None:
-        pass
+    def add_node(self, node: Node) -> None:
+        self._nodes[node.name] = node
+
+    def add_edge(self, from_node: str, to_node: str) -> None:
+        self._edges.append((from_node, to_node))
 
     def add_subgraph(self, subgraph: SubGraph) -> None:
-        pass
-
-    def compile(self) -> CompiledGraph:
-        pass
+        self._subgraphs.append(subgraph)
